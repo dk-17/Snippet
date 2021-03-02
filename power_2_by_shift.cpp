550d8e37a8947e9965a88168f0659e5c80fc839a
dk-17 <68947238+dk-17@users.noreply.github.com>
2021-03-02 19:07:21 +0530

    Create power_2_by_shift.cpp

---
+++ power_2_by_shift.cpp
@@ -0,0 +1,24 @@
+// Divya Kambaria
+#include<bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define mod 1000000007
+using namespace std;
+int main()
+{
+     #ifndef ONLINE_JUDGE
+       freopen("input.txt", "r", stdin);
+       freopen("output.txt", "w", stdout);
+     #endif
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    ll t; cin>>t;while(t--){
+    	int temp= 1<<24;
+   		
+
+   
+    
+}
+	
+}
